1b6a36b4b83471c3a00af3817491837b4a57cb81
SD Davis <davis@devcamp.com>
2017-08-08 17:23:25 +0000

    Added guest user  for curren_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -44,14 +44,14 @@ def show
   @portfolio_item = Portfolio.find(params[:id])
 end
 
-  def destroy
+def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
-    
-    #Destroy/delete the record
+
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
-    #Redirect
+
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
